a4bacd940e1fa4e96d547a8b2ffe47073fc21115
luckman212 <1992842+luckman212@users.noreply.github.com>
2023-09-07 14:17:31 -0400

    bash scripts update for workflow V3
    add +x fix for helper scripts, and more efficient checking
    compat with workflow 3.0.2

--- workflow/fix_flags.sh
+++ workflow/fix_flags.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+read -r 2>/dev/null WF_VER <fix_flags_sentinel
+[[ $alfred_workflow_version == "$WF_VER" ]] && exit
 cd "$(dirname "$0")" || exit 1
 /usr/bin/xattr -d com.apple.quarantine bitwarden-alfred-workflow 2>/dev/null
-/bin/chmod +x bitwarden-alfred-workflow 2>/dev/null
+/bin/chmod +x bitwarden-alfred-workflow bw_auto_lock.sh bw_cache_update.sh 2>/dev/null
+if [[ -n $alfred_workflow_version ]]; then
+  echo "$alfred_workflow_version" >fix_flags_sentinel
+fi
